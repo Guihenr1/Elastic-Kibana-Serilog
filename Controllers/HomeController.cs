4f381fcaabfd5831f654d54ee6f875f34a49d12b
Guilherme <guilherme.pomp@gmail.com>
2020-05-14 11:48:24 -0300

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Logs.Models;
+
+namespace Logs.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("HomeController executado em {date}", DateTime.UtcNow);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            try
+            {
+                var number = Convert.ToInt32("A");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Erro ao tentar acessar a aba Privacidade");
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
